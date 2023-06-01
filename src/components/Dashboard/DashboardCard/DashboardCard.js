3e0aae376e3d75b1bcbbc4e84d5a780bb1d66608
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-06-01 12:21:45 -0700

    Display individual article details and styling

--- src/components/Dashboard/DashboardCard/DashboardCard.js
+++ src/components/Dashboard/DashboardCard/DashboardCard.js
@@ -1,11 +1,21 @@
 import './DashboardCard.css';
+import { DateTime } from "luxon";
 
 export const DashboardCard = ({article}) =>  {
-  console.log(article)
+  const cleanName = article.source.name ? <p>Source: {article.source.name}</p> : null;
+  const cleanDate = DateTime.fromISO(article.publishedAt).toLocaleString(DateTime.DATE_MED);
+  
   return (
     <div className="dashboard-card">
       <h2>{article.title}</h2>
-      <p>{article.description}</p>
+      <section className='card-contents'>
+        <img src={article.urlToImage} alt={article.title}/>
+        <div className='card-description'>
+          <p>{article.description}</p>
+          {cleanName}
+          <p>Published: {cleanDate}</p>
+        </div>
+      </section>
     </div>
   );
 }
