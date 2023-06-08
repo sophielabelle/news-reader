9e15be231c7a1aa8253a19634bd9b7b1ee6ade0e
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-06-08 12:06:52 -0700

    Update fetch url to popular trending articles

--- src/data/apiCalls.js
+++ src/data/apiCalls.js
@@ -1,5 +1,5 @@
-export const apiRequest = () => {
-  return fetch(`https://newsapi.org/v2/everything?q=Apple&from=2023-06-07&sortBy=popularity&apiKey=de01d880c91b40fcab375895897a7b08`)
+export const fetchData = () => {
+  return fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=de01d880c91b40fcab375895897a7b08`)
   .then(res => {
     if(res.ok) {
       return res.json(); 
